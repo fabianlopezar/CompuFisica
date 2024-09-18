093e0ae9faabb4e8920c99845036d8f3926e7003
Fabian Lopez <65512494+fabianlopezar@users.noreply.github.com>
2024-09-17 22:27:35 -0500

    [ADD] [FL] Codigo Topo.

---
+++ Assets/Scripts/Diglett.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Diglett : MonoBehaviour
+{
+    public Animator animator;
+    public float time = 0;
+    void Start()
+    {
+        time = 10;
+    }
+  
+    void Update()
+    {
+        time = time - Time.deltaTime;
+        if (time <= 0)
+        {
+            Mostrar();
+            time = 3;
+            Esconder();
+        }
+    }
+    void Mostrar()
+    {
+        Debug.Log("Mostrar");
+        animator.Play("animMostrarTopo");
+    }
+    void Esconder()
+    {
+        Debug.Log("Esconder");
+        animator.Play("animEsconderTopo");
+    }
+    void Esperar()
+    {
+        animator.Play("animEsperarTopo");
+        Debug.Log("Esperar");
+    }
+    void AnimarGolpe()
+    {
+        Debug.Log("AnimarGolpe");
+    }
+    void SumarPunto()
+    {
+        Debug.Log("SumarPunto");
+    }
+}
