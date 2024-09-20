c218df384954b914d1b5d82d42b793893d705c04
Fabian Lopez <65512494+fabianlopezar@users.noreply.github.com>
2024-09-19 21:26:36 -0500

    [ADD] [FL] Agrege Controller Topos.

---
+++ Assets/Scripts/ControlPersonaje.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//video 10 https://www.youtube.com/watch?v=heXze1q5OBA&list=PLJMnJZL7OKtHQkrqG7oL-1o48kXA0YBgV&index=10
+
+public class ControlPersonaje : MonoBehaviour
+{
+    public GameObject[] topos;
+    public int randomValue=0;
+
+    void Start()
+    {
+        randomValue = Random.Range(0, 8);
+        StartCoroutine("lanzarTopo");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+    IEnumerator lanzarTopo()
+    {
+        topos[randomValue].GetComponent<Diglett>().Mostrar();
+        yield return new WaitForSeconds(2);
+        topos[randomValue].GetComponent<Diglett>().Esconder();
+        yield return new WaitForSeconds(2);
+        randomValue = Random.Range(0, 8);
+        StartCoroutine("lanzarTopo");
+        
+    }
+}
--- Assets/Scripts/Diglett.cs
+++ Assets/Scripts/Diglett.cs
@@ -6,41 +6,32 @@ public class Diglett : MonoBehaviour
 {
     public Animator animator;
     public float time = 0;
-    void Start()
-    {
-        time = 3;
-    }
+   
   
     void Update()
     {
-        time = time - Time.deltaTime;
-        if (time <= 0)
-        {
-            Mostrar();
-            time = 3;
-            Esconder();
-        }
+        
     }
-    void Mostrar()
+    public void Mostrar()
     {
         Debug.Log("Mostrar");
         animator.Play("animMostrarTopo");
     }
-    void Esconder()
+    public void Esconder()
     {
         Debug.Log("Esconder");
         animator.Play("animEsconderTopo");
     }
-    void Esperar()
+    public void Esperar()
     {
         animator.Play("animEsperarTopo");
         Debug.Log("Esperar");
     }
-    void AnimarGolpe()
+    public void AnimarGolpe()
     {
         Debug.Log("AnimarGolpe");
     }
-    void SumarPunto()
+    public void SumarPunto()
     {
         Debug.Log("SumarPunto");
     }
